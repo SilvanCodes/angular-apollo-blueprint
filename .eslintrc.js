c33615dc606929e2aab8e77af7079a0c452cfb6e
Silvan Buedenbender <s.buedenbender@badgersystems.de>
2020-05-11 12:02:44 +0200

    Add jest and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,79 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json"
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript"
+    ],
+    rules: {
+        /*=== basic rules ===*/
+        "space-in-parens": [
+            "warn",
+            "never",
+        ],
+        "space-before-blocks": [
+            "error",
+            "always"
+        ],
+        "keyword-spacing": [
+            "error",
+            {
+                before: true,
+                after: true
+            }
+        ],
+        "function-call-argument-newline": [
+            "warn",
+            "consistent"
+        ],
+        "object-curly-spacing": [
+            "error",
+            "always",
+            {
+                arraysInObjects: false,
+                objectsInObjects: false
+            }
+        ],
+        "no-multiple-empty-lines": [
+            "error",
+            {
+                max: 2,
+                maxBOF: 0,
+                maxEOF: 1
+            }
+        ],
+
+        /*=== typescript rules ===*/
+        "@typescript-eslint/no-empty-function": [
+            "error",
+            { allow: ["constructors"] }
+        ],
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            {
+                args: "all",
+                argsIgnorePattern: "^_"
+            }
+        ],
+        "@typescript-eslint/array-type": [
+            "error",
+            {
+                default: "generic"
+            }
+        ],
+
+        /*=== import rules ===*/
+        "import/newline-after-import": [
+            "error",
+            { count: 2 }
+        ]
+    }
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,35 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { Spectator, createHostFactory } from '@ngneat/spectator/jest';
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+
+  let spectator: Spectator<AppComponent>;
+  const createComponent = createHostFactory({
+    component:  AppComponent,
+    mocks: [
+      Router
+    ],
+    // in order to ignore non-stubbed router-outet
+    schemas: [ NO_ERRORS_SCHEMA ]
+  });
+
+  beforeEach(() => spectator = createComponent('<app-root></app-root>'));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = spectator.component;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'blueprint'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = spectator.component;
     expect(app.title).toEqual('blueprint');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('blueprint app is running!');
+    spectator.detectChanges();
+    expect(spectator.query('.content span').textContent).toContain('blueprint app is running!');
   });
 });
